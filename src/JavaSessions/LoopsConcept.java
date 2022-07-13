5f999d8f3767cbf5d3853da2cd180400671ca98d
Adith bala <adithbala@Adiths-MacBook-Pro.local>
2022-07-13 19:49:58 +0530

    130722_01

--- src/JavaSessions/LoopsConcept.java
+++ src/JavaSessions/LoopsConcept.java
@@ -32,10 +32,17 @@ public class LoopsConcept {
 		//facebook/linkedin app: lazy page loading
 		//use cases: we have to use while loop when we are not sure about number of cycles/iterations
 			//pagination 
-			//carousle
+			//carousel
 			//calendar handle
 
 		//2. for loop:
+			//use cases:
+				//we have to use for loop when we are sure about number of cycles/iterations
+				//menu items
+				//total number of links/images on the page
+				//drop down values
+				//webtable  -- rows/cols
+		
 		//typical for loop: (index based for loop)
 		//1 to 10:
 //		for(int p=1; p<=10; p++) {
@@ -55,12 +62,94 @@ public class LoopsConcept {
 			p=p+1;
 		}
 	
-		//infinite for loop:
-		for(int g=1; g<=10; ) {
-			System.out.println(g);
+		//1. infinite for loop:
+//		for(int g=1; g<=10; ) {
+//			System.out.println(g);
+//		}
+		
+		//2.
+//		for(;;) {
+//			System.out.println("Welcome to hotel taj");
+//		}
+		
+		System.out.println("----------------------------------------");
+		
+		//print even numbers up to 10:
+		for(int even=2; even<=10; even=even+2) {
+			System.out.println(even);
 		}
 		
-
+		System.out.println("----------------------------------------");
+		
+		for(int odd=1; odd<=10; odd=odd+2) {
+			System.out.println(odd);
+		}
+		
+		System.out.println("----------------------------------------");
+		for(double d=1.0; d<=10.0; d++) {
+			System.out.println(d);
+		}
+		
+		System.out.println("----------------------------------------");
+		//A-Z 
+		for(char c='A'; c<='Z';c++) {
+			System.out.println(c);
+		}
+		
+		//print ascii value equivalent char:
+		System.out.println("----------------------------------------");
+		for(int as=97; as<=122; as++) {
+			System.out.println((char)as);
+		}
+		
+		System.out.println("----------------------------------------");
+		System.out.println(10%2); //0 (remainder)
+		System.out.println(10%4); //2
+		
+		System.out.println("----------------------------------------");
+		
+		for(int t=1; t<=100; t++) {
+			System.out.println(t);
+			if(t%5==0) {
+				System.out.println("hi");
+			}
+		}
+		
+		//print even numbers with % operator:
+			//11(11%2==1)
+			//10(10%2==)
+		
+		for(int m=2; m<=20; m++) {
+			if(m%2==0) {
+				System.out.println(m);
+			}
+		}
+		
+		System.out.println("----------------------------------------");
+		
+		//do-while:
+		//use case:
+			//wait -- custom wait
+			//element is displayed
+		int r=1;
+		do {
+			r++;
+			System.out.println(r);
+			break;
+		} 
+		while (r<=10);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
