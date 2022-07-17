f163949e7dee8e005009ba76d4b4bbc2ecf3c6dc
adith <adith@adiths-Mac-mini.local>
2022-07-17 23:16:48 +0530

    170722_01

---
+++ src/JavaSessions/Car.java
@@ -0,0 +1,51 @@
+package JavaSessions;
+
+public class Car {
+	
+	String name;
+	int price;
+	String color;
+	static int wheels=4;
+	
+	//static class vars should be the common value
+	
+	//cannot use static in the main method
+	//all static vars of the class will be stored inside the 
+		//CMA(Common Memory Allocation/Permanent Generation/jdk 1.8 -> metaspace dynamic)
+	
+
+	public static void main(String[] args) {
+		
+		Car c1 = new Car();
+		c1.name="BMW";
+		c1.price=60;
+		c1.color="Blue";
+		System.out.println(c1.name+" "+c1.price+" "+c1.color);
+		
+		//how to access static variables
+		// no need to create the object
+		//1. within the same class, you can access them directly
+		//2. you can call by the class name:
+		//3. you can call by object reference:
+		
+		System.out.println(wheels);
+		
+		
+		Car c2 = new Car();
+		c2.name="Audi";
+		c2.price=70;
+		c2.color="White";
+		System.out.println(c2.name+" "+c2.price+" "+c2.color);
+		System.out.println(Car.wheels);
+		
+		Car c3 = new Car();
+		c3.name="honda";
+		c3.price=15;
+		c3.color="Black";
+		System.out.println(c3.name+" "+c3.price+" "+c3.color);
+		System.out.println(c3.wheels);
+		
+
+	}
+
+}
--- src/JavaSessions/ObjectReferenceConcept.java
+++ src/JavaSessions/ObjectReferenceConcept.java
@@ -2,9 +2,12 @@ package JavaSessions;
 
 public class ObjectReferenceConcept {
 	
+	//class data members:
+	//class vars
 	String name;
 	int age;
 	
+	//class methods:
 	public void get() {
 		System.out.println("get method...");
 	}
@@ -12,12 +15,25 @@ public class ObjectReferenceConcept {
 	public static void main(String[] args) {
 	
 		ObjectReferenceConcept obj = new ObjectReferenceConcept();
-		obj.name="Tom";
-		obj.age=25;
 		
-		System.out.println(obj.name+" : " +obj.age);
+		//object reference to null:
+//		obj = null;
+//		
+//		obj.name="Tom"; //java.lang.NullPointerException
+//		obj.age=25;
+//		
+//		System.out.println(obj.name+" " +obj.age); 
 		
-		obj.get();
+		//no reference object:
+		new ObjectReferenceConcept().name="Peter";
+		new ObjectReferenceConcept().age=25;
+		new ObjectReferenceConcept().get();
+		
+		ObjectReferenceConcept obj1 = new ObjectReferenceConcept();
+		
+		System.gc();
+		
+		
 	}
 
 }
