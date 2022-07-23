8b06e2b84711fd8179b471e77aacc8bbc6e91bb5
adith442864 <adith442864@gmail.com>
2022-07-23 12:58:31 +0530

    230722

--- src/ExceptionHandling/TryCatchBlock.java
+++ src/ExceptionHandling/TryCatchBlock.java
@@ -1,17 +1,44 @@
 package ExceptionHandling;
 
 public class TryCatchBlock {
-
+	
+	String name;
+	
 	public static void main(String[] args) {
 		
 		System.out.println("A");
 		System.out.println("A");
 		System.out.println("A");
 		
-		int i=9/0;
+		TryCatchBlock obj = new TryCatchBlock();
+		obj.name="Tom";
+		//obj = null; //NullPointerException
+		
+		try {
+			System.out.println(obj.name);//NPE
+			int i = 9/0;
+			System.out.println("hii");
+			System.out.println("hii");
+			System.out.println("hii");
+		}
+		catch(ArithmeticException e) {
+			System.out.println("Arithmetic Exception occured....");
+			e.printStackTrace();
+		}
+		catch(NullPointerException e) {
+			System.out.println("NullPointer Exception occured....");
+			e.printStackTrace();
+		}
+//		catch(Exception e) {
+//			System.out.println("some exception is coming...");
+//			e.printStackTrace();
+//		}
+		
 		System.out.println("B");
 		System.out.println("B");
 		System.out.println("B");
+		
+		
 
 	}
 
