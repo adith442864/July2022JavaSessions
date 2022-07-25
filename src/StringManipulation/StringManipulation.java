c1fc09ceb89ffc3400609ff3ac8764fc820c9582
adith442864 <adith442864@gmail.com>
2022-07-25 20:56:21 +0530

    250722_01

--- src/StringManipulation/StringManipulation.java
+++ src/StringManipulation/StringManipulation.java
@@ -41,16 +41,84 @@ public class StringManipulation {
 		String dob = "01-01-1990"; //01/01/1990
 		System.out.println(dob.replace("-", "/"));
 		
+		//contains
+		String s1 = "your user id is naveenautomation";
+		System.out.println(s1.contains("naveenautomation"));
+		if(s1.contains("naveenautomation")) {
+			System.out.println("PASS");
+		}
+		
+		//comparison:
+		String t1 = "hello java"; //string literals
+		String t2 = "hello java";
+		
+		System.out.println(t1.equals(t2)); //true
+		System.out.println(t1.equalsIgnoreCase(t2)); //true
+		System.out.println(t1);
+		System.out.println(t2);
+		System.out.println(t1==t2); //comparing two object values:
+		
+		//interviewQ: after JDK 1.7 (SCP is part of HEAP Memory)
+		String t3 = new String("hello ruby"); //new keyword -- object in heap - 2 objects(SCP + HEAP)
+		String t4 = new String("hello ruby"); //1(heap)
+		String t5 = "hello ruby"; //0
+		System.out.println(t3==t4); //false --> comparing two object references
+		System.out.println(t4==t5); //false
+		
+		//split()
+		String lang = "JAVA_PYTHON_RUBY_PHP";
+		String langArr[] = lang.split("_");
+		System.out.println(langArr[0]);
+		//System.out.println(langArr[4]);
+		//for each loop:
+		for(String e: langArr) {
+			System.out.println(e);
+		}
+		
+		String name = "xXtestingxXjavaxXXSeleniumXxXPython";
+		String nameArr[] = name.split("xX");
+		System.out.println(nameArr[0]); //blank value
+		System.out.println(nameArr[1]); //testing
+		System.out.println(nameArr[3]); //XSeleniumX
+		//for each:
+		for(String e: nameArr) {
+			System.out.println(e);
+		}
+		
+		String empData = "Hiren;Patel;40;NY;USA;01-01-1970;selenium;QA";
+		String empDataArr[] = empData.split(";");
+		for(String e: empDataArr) {
+			System.out.println(e);
+		}
+		
+		String credentials = "admin:admin123";
+		System.out.println(credentials.split(":")[0]); //admin
+		System.out.println(credentials.split(":")[1]); //admin123
+		
+		//substring() :
+		String message = "your order is 12345";
+		System.out.println(message.substring(8));
+		System.out.println(message.substring(17));
+		System.out.println(message.substring(0, 10));
+		System.out.println(message.substring(message.indexOf("is")+3, message.length()));
+		String orderId = message.substring(message.indexOf("is")+3, message.length());
+		System.out.println(orderId);
 		
 		
+		//interviewQ
+		String text = "hOW aRe yOu";
+		String textArr[] = text.split(" ");
+		
+		String remaining="";
+		for(String words : textArr) {
+			String first = text.substring(0,1);
+			String second = text.substring(1).toLowerCase();
+			remaining = first.toUpperCase() + second +" ";
+		}
+		
+		System.out.println(remaining);
 		
 		
-		
-		
-		
-		
-		
-
 	}
 
 }
